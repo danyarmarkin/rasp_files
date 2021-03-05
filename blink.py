61a767657e3792cc037a39a32bc9f1f53e911971
Danila Yarmarkin <danila.iarmarkin@gmail.com>
2021-03-05 20:42:48 +0300

    init

---
+++ blink.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+n = 1
+
+while (True):
+    for i in range(n):
+         GPIO.output(18, True)
+         time.sleep(0.1)
+         GPIO.output(18, False)
+         time.sleep(0.2)
+    time.sleep(0.3)
+    n = n + 1 
+
